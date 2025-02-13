1bda54a502678804b0f2c183de9c365e62114836
Sah2Sah2 <sara.battistella@iths.se>
2025-02-13 15:19:06 +0100

    Stop tracking config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    API_URL: 'https://nintendogamesminimalapi.azurewebsites.net/games',
-    SEARCH_URL: 'https://nintendogamesminimalapi.azurewebsites.net/game?name=',
-    ADD_GAME_URL: 'https://nintendogamesminimalapi.azurewebsites.net/game'
-};
